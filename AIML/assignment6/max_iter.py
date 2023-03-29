bbc8d8ccb0edc8d92c2ffe1317aaf5f4e9c340ea
Indranil Saha <sahaindranil2004@gmail.com>
2023-03-30 00:25:28 +0530

    Added: AIML6

--- AIML/assignment6/max_iter.py
+++ AIML/assignment6/max_iter.py
@@ -5,7 +5,7 @@
 
 import pandas as pd
 from sklearn.datasets import load_boston
-from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 boston = load_boston()
@@ -21,19 +21,19 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 max_iter_values = range(10000, 100001, 10000)
 
-r2_scores = []
-for max_iter in max_iter_values:
-    model = SGDRegressor(max_iter=max_iter)
+max_r2_score = 0
+max_iter = 0
+
+for i in max_iter_values:
+    model = LinearRegression()
     model.fit(x_train, y_train)
     r2_score = model.score(x_test, y_test)
-    r2_scores.append(r2_score)
-
-max_r2_score = max(r2_scores)
-max_iter_index = r2_scores.index(max_r2_score)
-max_iter = max_iter_values[max_iter_index]
+    if r2_score > max_r2_score:
+        max_r2_score = r2_score
+        max_iter = i
 
 print(
     f"Maximum R^2 score of {max_r2_score} obtained with max_iter={max_iter}.")
 
 # OUTPUT
-# Maximum R^2 score of 0.30156333023278636 obtained with max_iter=80000.
+# Maximum R^2 score of 0.3707569232254778 obtained with max_iter=10000.
